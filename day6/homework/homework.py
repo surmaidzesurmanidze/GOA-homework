709f2a324edf4cb4a3586dc536c7f0e663f19884
surmaidzesurmanidze <gameplayonly76@gmail.com>
2024-03-13 20:21:52 +0400

    commit

---
+++ day6/homework/homework.py
@@ -0,0 +1,29 @@
+input("your name")
+input("your surmame")
+input("your age")
+
+
+
+
+
+
+input(12)
+input(10)
+
+print(12+10)
+print(12-10)
+print(12/10)
+print(12*10)
+
+
+
+
+
+
+
+input("rusiko")
+input("giorgi")
+input("revazi")
+input("temo")
+
+print(44+25,56+25,17+25,15+25)

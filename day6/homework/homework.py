59365e3a33d65fed0597f8b797978ffd610525f4
surmaidzesurmanidze <gameplayonly76@gmail.com>
2024-03-15 20:39:13 +0400

    commit

--- day6/homework/homework.py
+++ day6/homework/homework.py
@@ -12,8 +12,8 @@ print("this person is ",name,surname,"he/she is",age,"years old","he/she lives i
 
 
 
-num1 = input("enter first number")
-num2 = input("enter second number")
+num1 = int(input("enter first number"))
+num2 = int(input("enter second number"))
 print(num1*num2)
 print(num1+num2)
 print(num1-num2)

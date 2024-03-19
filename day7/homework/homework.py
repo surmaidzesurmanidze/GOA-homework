462472a5572ef3651ed969ed5009549e34d6d2c8
surmaidzesurmanidze <gameplayonly76@gmail.com>
2024-03-19 20:28:39 +0400

    commit

--- day7/homework/homework.py
+++ day7/homework/homework.py
@@ -11,3 +11,14 @@ print(type(data3))
 
 
 
+num1 = int(input("first number"))
+num2 = int(input("second number"))
+num3 = int(input("thirs number"))
+num4 = int(input("fourth number"))
+num5 = int(input("fith number"))
+
+print(num1*num2*num3*num4*num5)
+print(num1/num2/num3/num4/num5)
+print(num1-num2-num3-num4-num5)
+print(num1+num2+num3+num4+num5)
+
